270d5bda8abdd12df07b02d4fbc5150121f63c65
Jonathan Bramble <jbramble82@gmail.com>
2014-10-31 16:58:05 +0000

    update dir names and add roundness chart

--- clusterplot.R
+++ clusterplot.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 
-experiment = "PYT4001"
-dir = "~/Data/Confocal/PC + PYRONE ET 281014/PYExp10/processed/"
+experiment = "PYE4002"
+dir = "~/Data/Confocal/PC_PYRONE_ET_281014/PYExp12/processed/"
 
-file_root = paste(dir,"AVG_PYExp10.lei - ",sep = "")
+file_root = paste(dir,"AVG_PYExp12.lei - ",sep = "")
 file_name = paste(file_root,experiment,sep = "")
 
 #imgdat <- list()
@@ -15,7 +15,10 @@ imgdat5 <- read.csv(paste(file_name,"-5-dat.csv",sep = ""))
 imgdat6 <- read.csv(paste(file_name,"-6-dat.csv",sep = ""))
 imgdat7 <- read.csv(paste(file_name,"-7-dat.csv",sep = ""))
 
-#imgdat <- do.call(rbind, list(imgdat1,imgdat2,imgdat3,imgdat4,imgdat5,imgdat6))
+
+#imgdat <- do.call(rbind, list(imgdat1,imgdat2,imgdat3))
+#imgdat <- do.call(rbind, list(imgdat1,imgdat2,imgdat3,imgdat5,imgdat6))
+#imgdat <- do.call(rbind, list(imgdat1,imgdat2,imgdat3,imgdat4,imgdat5))
 imgdat <- do.call(rbind, list(imgdat1,imgdat2,imgdat3,imgdat4,imgdat5,imgdat6,imgdat7))
 
 imgframe <- data.frame(imgdat)
@@ -24,15 +27,22 @@ chart.binwidth <- 1
 chart.title <- paste("Domain Area",experiment)
 
 p <- ggplot(data=imgframe,aes(x=Area))
-p + geom_histogram(binwidth=chart.binwidth) + xlab(expression(paste("Area, ",um^2))) +labs(title = chart.title) + theme_minimal(base_size = 16) +labs(title = chart.title) 
-file_name = paste(experiment,"area.png")
-full_path = paste(dir,file_name)
-ggsave(full_path)
-
+p + geom_histogram(binwidth=chart.binwidth) + ylab("Count") + xlab(expression(paste("Area, ",um^2))) +labs(title = chart.title) + theme_minimal(base_size = 20) +labs(title = chart.title) 
+file_name = paste(experiment,"area.png",sep="_")
+full_path = paste(dir,file_name,sep="")
+ggsave(full_path,width=8,height=8)
 
 chart.title <- paste("Domain Feret Diameter",experiment)
 q <- ggplot(data=imgframe,aes(x=Feret))
-q + geom_histogram(binwidth=chart.binwidth) + xlab(expression(paste("Feret Diameter, ",um))) +labs(title = chart.title) + theme_minimal(base_size = 16) +labs(title = chart.title)
-file_name = paste(experiment,"feret.png")
-full_path = paste(dir,file_name)
-ggsave(full_path)
+q + geom_histogram(binwidth=chart.binwidth) + ylab("Count") + xlab(expression(paste("Feret Diameter, ",um))) +labs(title = chart.title) + theme_minimal(base_size = 20) +labs(title = chart.title)
+file_name = paste(experiment,"feret.png",sep="_")
+full_path = paste(dir,file_name,sep="")
+ggsave(full_path,width=8,height=8)
+
+chart.title <- paste("Domain Area",experiment)
+file_name = paste(experiment,"round.png",sep="_")
+full_path = paste(dir,file_name,sep="")
+png(full_path, width=10, height=10, units="cm", res=300)
+par(mar=c(4,4,4,4))
+plot(imgdat$Area,imgdat$Round,xlab="Domain Area",ylab="Roundness",main=chart.title)
+dev.off()
