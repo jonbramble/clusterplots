f2bd83c5296828752f64f02059d20a14f988cef9
Jonathan Bramble <jbramble82@gmail.com>
2014-11-13 17:03:10 +0000

    updated load code

--- domains.R
+++ domains.R
@@ -1,6 +1,6 @@
 library(ggplot2)
-rm(list = ls())
-dir = "/media/mbajb/My Passport/Jonathan/Nottingham/PC + PYRONE ZE 051114"
+#rm(list = ls())
+dir = "/media/mbajb/My Passport/Jonathan/Nottingham/PC + PYRONE ZE 051114/"
 data.sources = list.files(dir,pattern="*.rda")
 sapply(data.sources,load,.GlobalEnv)
 
@@ -9,11 +9,12 @@ lnames <- lapply(lfiles,as.name)
 ldat <- lapply(lnames,eval)
 
 PYA <- do.call(rbind,ldat)
+save(PYA,file="PYA.rda")
 
-chart.binwidth <- 1
-chart.title <- paste("Domain Area")
+#chart.binwidth <- 1
+#chart.title <- paste("Domain Area")
 
-p <- ggplot(data=PYA,aes(x=Area,fill=Experiment))
-p + geom_bar(binwidth=chart.binwidth) + xlim(0, 50) + ylab("Count") + xlab(expression(paste("Area, ",um^2))) +labs(title = chart.title) + theme_minimal(base_size = 20) +labs(title = chart.title) 
+#p <- ggplot(data=PYA,aes(x=Area,fill=Experiment))
+#p + geom_bar(binwidth=chart.binwidth) + xlim(0, 50) + ylab("Count") + xlab(expression(paste("Area, ",um^2))) +labs(title = chart.title) + theme_minimal(base_size = 20) +labs(title = chart.title) 
 
-area_data <- PYA$Area
+#area_data <- PYA$Area
