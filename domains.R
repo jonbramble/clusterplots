695d44acf2cf7b2578f1237d6118183128a6957a
Jonathan Bramble <jbramble82@gmail.com>
2014-11-14 18:07:42 +0000

    added more data wraggling

--- domains.R
+++ domains.R
@@ -1,11 +1,11 @@
-rm(list = ls())
+#rm(list = ls())
 library(ggplot2)
 library(plyr)
 source('~/Programming/R/clusterplot/config.R')
 source('~/Programming/R/clusterplot/compound.R')
 source('~/Programming/R/clusterplot/exp_match.R')
 
-dir = "/media/mbajb/My Passport/Jonathan/Nottingham/PC + PYRONE ZE 051114"
+dir = "/media/mbajb/My Passport/Jonathan/Nottingham/common"
 data.sources = list.files(dir,pattern="*.rda")
 full_path_sources = paste(dir,data.sources,sep="/")
 sapply(full_path_sources,load,.GlobalEnv)
@@ -24,16 +24,15 @@ chart.title <- paste("Domain Area")
 #process names to fill in the extra columns
 PYA$Compound <- substr(PYA$Experiment,3,3) #extract from string
 codes <- as.integer(substr(PYA$Experiment,4,4))
-PYA$Sample <- as.integer(substr(PYA$Experiment,7,7))
-PYA$Amount <- sapply(codes,compound)
-PYA$Session <- sapply(PYA$Experiment,exp_match)
-PYA$ImageCode <- paste(PYA$Experiment,PYA$Batch,sep="-")
+PYA$Sample <- as.integer(substr(PYA$Experiment,7,7))  #get the sample number
+PYA$Amount <- sapply(codes,compound)  #compound function converts to correct amount
+PYA$Session <- sapply(PYA$Experiment,exp_match) #find the session from the experiment name from config array
+PYA$ImageCode <- paste(PYA$Experiment,PYA$Batch,sep="-") #create unique image code
 
 dis <- as.data.frame(table(PYA$ImageCode), responseName="Count")
 colnames(dis) <- c("ImageName","Count")
 dis$Session <- sapply(dis$ImageName,session_match)
-dis$path <- paste()
-
+dis$path <- paste(paste('"',paste(dir,dis$Session,'processed',paste(dis$ImageName,'-mask"',sep=''),sep='/'),sep=''),"png",sep=".")
 
 
 p <- ggplot(data=PYA,aes(x=Area,fill=factor(Amount)))
