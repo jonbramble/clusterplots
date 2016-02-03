478f45c1097bd8d7ccaa92977d62ae688450dbae
Jonathan Bramble <jbramble82@gmail.com>
2016-02-03 17:04:42 +0000

    added labels and saving

--- generic_process.R
+++ generic_process.R
@@ -1,10 +1,12 @@
 library(ggplot2)
+library(plyr)
 
 #assumes a single dir containing dirs with each lei file and a processed dir
 #path in windows must be escaped c://path//to//folder
 
 #data_dir <- "/media/mbzjpb/data/Experimental/Confocal/PolyQ"
-data_dir <- "E:\\Confocal Data\\Processed Data\\FPE_PC_NaSCN\\processed"
+data_dir <- "/media/mbzjpb/data/Experimental/Confocal/Processed Data/FPE_PC_NaSCN/processed"
+#data_dir <- "E:\\Confocal Data\\Processed Data\\FPE_PC_NaSCN\\processed"
 #get all the directories in this folder
 data_dirs <- list.files(data_dir)  
 #construct a file path to the processed dir
@@ -23,26 +25,26 @@ df_nascn <- subset(df_filter,media == 'NaSCN')
 count(df_pbs$lipid)
 count(df_nascn$lipid)
 
+label_axis <- expression(paste("Area (",mu,m^{2},")"))
+
 #plotting
 
 #m <- ggplot(data=subset(df_filter,Lipid=="PC"),aes(x=Area,fill=factor(Lipid)))
 #m + geom_bar(position="dodge",binwidth=1) + xlim(0,50) + theme_minimal(base_size = 24)
 
 m <- ggplot(data=df_pbs,aes(x=Area,fill=factor(lipid)))
-m + geom_bar(position="dodge",binwidth=2) + xlim(0,20) + theme_minimal(base_size = 24)
+m + geom_histogram(position = "dodge",binwidth=2) + xlim(0,20) + theme_minimal(base_size = 24) +
+ xlab(label_axis) + ggtitle("PBS") + scale_fill_discrete(name="Lipid",breaks=c("PC","PCChol","PCCholSM"))
+ggsave("/home/mbzjpb/Documents/Latex/jl_patterning/jl_feb16_hist_pbs.eps")
+
 
 n <- ggplot(data=df_nascn,aes(x=Area,fill=factor(lipid)))
-n + geom_bar(position="dodge",binwidth=2) + xlim(0,20) + theme_minimal(base_size = 24)
+n + geom_histogram(position="dodge",binwidth=2) + xlim(0,20) + theme_minimal(base_size = 24) + 
+  xlab(label_axis) + ggtitle("NaSCN") + scale_fill_discrete(name="Lipid",breaks=c("PC","PCChol","PCCholSM"))
+ggsave("/home/mbzjpb/Documents/Latex/jl_patterning/jl_feb16_hist_nascn.eps")
 
-p <- ggplot(data=subset(df_filter,Buffer=="PBS"),aes(x=Area,fill=factor(Lipid)))
-p + geom_bar(position="dodge",binwidth=1) + xlim(0,50) + theme_minimal(base_size = 24)
-
-df_time<- subset(df_filter,Buffer="Tris" & Type == "T")
-m <- ggplot(data=df_time,aes(x=Area,fill=factor(Time)))
-m + geom_bar(position="dodge",binwidth=5) +xlim(0,50) + theme_minimal(base_size = 24)
-
-p <- n + geom_density(alpha=0.3) + xlim(0,20) + ylim(0,0.5) + theme_minimal(base_size = 24)
-q <- m + geom_density(alpha=0.3) + xlim(0,20) + ylim(0,0.5) + theme_minimal(base_size = 24)
+p <- n + geom_density(alpha=0.3) + xlim(0,20) + ylim(0,1) + theme_minimal(base_size = 24)
+q <- m + geom_density(alpha=0.3) + xlim(0,20) + ylim(0,1) + theme_minimal(base_size = 24)
 p
 q
 
